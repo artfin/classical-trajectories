05bad8e508068a5832b219e6c08f341f6725c462
artfin <artfin@mail.ru>
2017-03-02 01:00:06 +0300

    interim

--- black-box/main.py
+++ black-box/main.py
@@ -3,6 +3,7 @@ sys.path.append('/home/artfin/Desktop/repos/sympy-project/sympy/lib') # path on 
 sys.path.append('/Users/mac/repos/sympy_project/sympy/lib') # path on mac
 
 from pprint import pprint
+from itertools import product
 
 from __particle__ import __particle__
 
@@ -48,19 +49,34 @@ particle_2 = __particle__(m = m, __x__ = lambda q: -r0 * np.cos(q/2),
 particles = [particle_1, particle_2]
 
 def hamiltonian(q = None, p = None, jx = None, jy = None, jz = None, effective_potential = False):
+	"""
+	q -- np.array object with all degrees of freedom
+	p -- np.array object with conjugate momentum
+	"""
+
 	J_vector = np.array([jx, jy, jz])
 	p_vector = np.array([p])
 
-	Ixx = sum([particle.m * (particle.__y__(q)**2 + particle.__z__(q)**2) for particle in particles])
-	Iyy = sum([particle.m * (particle.__x__(q)**2 + particle.__z__(q)**2) for particle in particles])
-	Izz = sum([particle.m * (particle.__x__(q)**2 + particle.__y__(q)**2) for particle in particles])
-	Ixy = - sum([particle.m * particle.__x__(q) * particle.__y__(q) for particle in particles])
-	Ixz = - sum([particle.m * particle.__x__(q) * particle.__z__(q) for particle in particles])
-	Iyz = - sum([particle.m * particle.__y__(q) * particle.__z__(q) for particle in particles])
+	Ixx = sum([particle.m * (particle.__y__(_deg)**2 + particle.__z__(_deg)**2) for _deg in q for particle in particles])
+	Iyy = sum([particle.m * (particle.__x__(_deg)**2 + particle.__z__(_deg)**2) for _deg in q for particle in particles])
+	Izz = sum([particle.m * (particle.__x__(_deg)**2 + particle.__y__(_deg)**2) for _deg in q for particle in particles])
+	Ixy = - sum([particle.m * particle.__x__(_deg) * particle.__y__(_deg) for _deg in q for particle in particles])
+	Ixz = - sum([particle.m * particle.__x__(_deg) * particle.__z__(_deg) for _deg in q for particle in particles])
+	Iyz = - sum([particle.m * particle.__y__(_deg) * particle.__z__(_deg) for _deg in q for particle in particles])
 	
 	inertia_tensor = np.array([[Ixx, Ixy, Ixz], [Ixy, Iyy, Iyz], [Ixz, Iyz, Izz]])
-	
+
+	pprint(inertia_tensor)
+
+	# a = np.zeros((q.shape[0], q.shape[0]))
+	# for j, k in product(range(q.shape[0]), range(q.shape[0])):
+	# 	a[j, k] = sum([particle.m * (particle.__dx__(q[j]) * particle.__dx__(q[k]) + \
+	# 							 	 particle.__dy__(q[j]) * particle.__dy__(q[k]) + \
+	# 								 particle.__dz__(q[j]) * particle.__dz__(q[k])) for particle in particles])
+
 	a = np.array([sum([particle.m * (particle.__dx__(q)**2 + particle.__dy__(q)**2 + particle.__dz__(q)**2) for particle in particles])])
+
+	pprint(a)
 
 	G11 = np.linalg.inv(inertia_tensor)
 	G22 = 1/a
@@ -96,13 +112,13 @@ print 'Energy: {0}'.format(E)
 
 qe = 1.503583924
 
-effective_potential = hamiltonian(q = qe, jx = Jx0, jy = Jy0, jz = Jz0, effective_potential = True)
+effective_potential = hamiltonian(q = np.array([qe]), jx = Jx0, jy = Jy0, jz = Jz0, effective_potential = True)
 print 'effective_potential: {0}'.format(effective_potential)
 
 pini = np.sqrt(I0 * (E - effective_potential))
 print 'pini: {0}'.format(pini)
 
-print 'hamiltonian: {0}'.format(hamiltonian(q = qe, p = pini, jx = Jx0, jy = Jy0, jz = Jz0))
+print 'hamiltonian: {0}'.format(hamiltonian(q = np.array([qe]), p = pini, jx = Jx0, jy = Jy0, jz = Jz0))
 
 dham_dq = grad_named(hamiltonian, argname = 'q')
 dham_dp = grad_named(hamiltonian, argname = 'p')
@@ -116,8 +132,8 @@ def fcdiff(func, vals, argnum, step = 0.00001):
 	_vals_m[argnum] = _vals_m[argnum] - step
 	return (func(*_vals_p) - func(*_vals_m)) / (2 * step)
 
-print 'automatic dH/dq: {0}'.format(dham_dq(qe, pini, Jx0, Jy0, Jz0))
-print 'numeric dH/dq: {0}'.format(fcdiff(func = hamiltonian, vals = [qe, pini, Jx0, Jy0, Jz0], argnum = 0))
+print 'automatic dH/dq: {0}'.format(dham_dq(np.array([qe]), pini, Jx0, Jy0, Jz0))
+print 'numeric dH/dq: {0}'.format(fcdiff(func = hamiltonian, vals = [np.array([qe]), pini, Jx0, Jy0, Jz0], argnum = 0))
 
 print 'automatic dH/dp: {0}'.format(dham_dp(qe, pini, Jx0, Jy0, Jz0))
 print 'numeric dH/dp: {0}'.format(fcdiff(func = hamiltonian, vals = [qe, pini, Jx0, Jy0, Jz0], argnum = 1))
@@ -133,17 +149,6 @@ print 'numeric dH/djz: {0}'.format(fcdiff(func = hamiltonian, vals = [qe, pini, 
 
 rhs = []
 
-# def dham_djx(q, p, theta, varphi):
-# 	return 1/(J * np.cos(theta) * np.cos(varphi)) * dham_dtheta(q, p, theta, varphi) - \
-# 		   1/(J * np.sin(theta) * np.sin(varphi)) * dham_dvarphi(q, p, theta, varphi)
-
-# def dham_djy(q, p, theta, varphi):
-# 	return 1/(J * np.cos(theta) * np.sin(varphi)) * dham_dtheta(q, p, theta, varphi) + \
-# 		   1/(J * np.sin(theta) * np.cos(varphi)) * dham_dvarphi(q, p, theta, varphi)
-
-# def dham_djz(q, p, theta, varphi):
-# 	return - 1/(J * np.sin(theta)) * dham_dtheta(q, p, theta, varphi)
-
 rhs = [lambda q, p, jx, jy, jz:   dham_dp(q, p, jx, jy, jz),
 	   lambda q, p, jx, jy, jz: - dham_dq(q, p, jx, jy, jz),
 	   lambda q, p, jx, jy, jz:   dham_djz(q, p, jx, jy, jz) * jy - dham_djy(q, p, jx, jy, jz) * jz,
@@ -153,14 +158,11 @@ rhs = [lambda q, p, jx, jy, jz:   dham_dp(q, p, jx, jy, jz),
 
 # rhs should return derivatives in the same order: q_dot, p_dot, theta_dot, varphi_dot
 def derivatives(t, y):
-	# print t, y
-	print dham_djy(y[0], y[1], y[2], y[3], y[4])
-	return [eq(y[0], y[1], y[2], y[3]) for eq in rhs]
+	#print t, y
+	print t, y[3] 
+	return [eq(y[0], y[1], y[2], y[3], y[4]) for eq in rhs]
 
 init = [qe, pini, Jx0, Jy0, Jz0]
-# print dham_djz(init[0], init[1], init[2], init[3])
-# t = np.linspace(0, 500, 500)
-# sol = odeint(derivatives, init, t, atol = 10**(-6))
 
 t_start = 0.
 t_end = 100.
@@ -168,34 +170,36 @@ t_step = 1.
 
 ode = spi.ode(derivatives)
 
-ode.set_integrator('lsoda', nsteps = 500, method = 'bdf', atol = 1e-6)
-ode.set_initial_value(init, t_start)
+# ode.set_integrator('lsoda', nsteps = 500, method = 'bdf', atol = 1e-6)
+# ode.set_initial_value(init, t_start)
 
-sol = []
-t = []
-while ode.successful() and ode.t < t_end:
-	ode.integrate(ode.t + t_step)
-	t.append(ode.t)
-	sol.append(ode.y)
+# sol = []
+# t = []
+# while ode.successful() and ode.t < t_end:
+# 	ode.integrate(ode.t + t_step)
+# 	t.append(ode.t)
+# 	sol.append(ode.y)
 
-
-# saving it just in case
-np.savetxt("simple-water.dat", sol)
 
 # q_list = extract_column(data = sol, column = 0)
 # p_list = extract_column(data = sol, column = 1)
-# theta_list = extract_column(data = sol, column = 2)
-# varphi_list = extract_column(data = sol, column = 3)
+# jx_list = extract_column(data = sol, column = 2)
+# jy_list = extract_column(data = sol, column = 3)
+# jz_list = extract_column(data = sol, column = 4)
+
+# saving it just in case
+# np.savetxt("simple-water.dat", jy_list)
 
 # plt.plot(t, q_list, 'b', label = 'q(t)')
 # plt.plot(t, p_list, 'g', label = 'p(t)')
 
-# plt.plot(t, theta_list, 'b', label = 'theta(t)')
-plt.plot(t, varphi_list, 'g', label = 'varphi(t)')
-plt.legend(loc = 'best')
-plt.xlabel('t')
-plt.grid()
-plt.show()
+# plt.plot(t, jx_list, 'g', label = 'jx(t)')
+# plt.plot(t, jy_list, 'b', label = 'jy(t)')
+# plt.plot(t, jz_list, 'r', label = 'jz(t)')
+# plt.legend(loc = 'best')
+# plt.xlabel('t')
+# plt.grid()
+# plt.show()
 
 # phi = np.linspace(0, 2 * np.pi, 100)
 # theta = np.linspace(0, np.pi, 100)
@@ -210,7 +214,7 @@ plt.show()
 # fig = plt.figure()
 # ax = plt.axes(projection='3d')
 
-# ax.plot(xx, yy, zz, '-b')
+# ax.plot(jx_list, jy_list, jz_list, '-b')
 # plt.tight_layout()
 # plt.show()
 
