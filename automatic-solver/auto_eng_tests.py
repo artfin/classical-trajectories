d3694fe8cb9f39c490e5c16bc7fdbbc73cc8744a
artfin <artfin@mail.ru>
2017-03-25 16:26:35 +0300

    automatic engine start

--- automatic-solver/auto_eng_tests.py
+++ automatic-solver/auto_eng_tests.py
@@ -7,7 +7,6 @@ from itertools import product
 from time import clock
 
 import numpy as np
-from numpy.linalg import inv
 
 class Lagrange(object):
     """ Lagrange class symbolically constructs matrices I, a, A and their derivatives. """
@@ -27,7 +26,6 @@ class Lagrange(object):
         Ixy = Add(*[- particle['m'] * particle['x'] * particle['y'] for particle in self.particles]).simplify()
         Ixz = Add(*[- particle['m'] * particle['x'] * particle['z'] for particle in self.particles]).simplify()
         Iyz = Add(*[- particle['m'] * particle['y'] * particle['z'] for particle in self.particles]).simplify()
-             
         return DenseMatrix([[Ixx, Ixy, Ixz], [Ixy, Iyy, Iyz], [Ixz, Iyz, Izz]])
 
     def calculate_a_matrix(self):
@@ -67,65 +65,6 @@ class Lagrange(object):
 
         return A            
 
-class Hamiltonian(object):
-    """ Evaluates hamiltonian and it's derivatives using hard-coded automatic differentiation formualae."""
-
-    def __init__(self, I, a, A):
-        
-        self.I, self.a, self.A = I, a, A
-
-        self.I_function = Lambdify([q1, q2], self.I)
-        self.a_function = Lambdify([q1, q2], self.a)
-        self.A_function = Lambdify([q1, q2], self.A)
-        
-        I_q1_function = Lambdify([q1, q2], self.I.diff(q1))
-        I_q2_function = Lambdify([q1, q2], self.I.diff(q2))
-        self.I_derivatives = np.array([I_q1_function, I_q2_function])
-
-        a_q1_function = Lambdify([q1, q2], self.a.diff(q1))
-        a_q2_function = Lambdify([q1, q2], self.a.diff(q2))
-        self.a_derivatives = np.array([a_q1_function, a_q2_function])
-
-        A_q1_function = Lambdify([q1, q2], self.A.diff(q1))
-        A_q2_function = Lambdify([q1, q2], self.A.diff(q2))
-        self.A_derivatives = np.array([A_q1_function, A_q2_function])
-
-    def G11_eval(self, q, Is_derivatives, as_derivatives, As_derivatives):
-        """
-            Input: 
-                q_values -- np.array()? of numerical values of freedom_degrees
-                matrix_derivatives -- np.arrays() of symbolic matrices (I,a, A derivatives
-        """
-
-        I = np.array(self.I_function(q))
-        a = np.array(self.a_function(q))
-        A = np.array(self.A_function(q))
-
-        a_inv = inv(a)
-        At = A.transpose()
-
-        G11 = inv(I - np.dot(A, np.dot(a_inv, At)))
-        
-        In_derivatives = []
-        for derivative in Is_derivatives:
-            In_derivatives.append(derivative(q))
-        
-        An_derivatives = []
-        for derivative in As_derivatives:
-            An_derivatives.append(derivative(q))
-
-        an_derivatives = []
-        for derivative in as_derivatives:
-            an_derivatives.append(derivative(q))
-        
-        G11_derivatives = []
-        for I_derivative, a_derivative, A_derivative in zip(In_derivatives, an_derivatives, An_derivatives):
-        
-            b1 = A_derivative - np.dot(A, np.dot(a_inv, a_derivative))
-            b2 = I_derivative - np.dot(b1, np.dot(a_inv, At))- np.dot(A, np.dot(a_inv, A_derivative.transpose()))
-
-        return G11,- np.dot(G11, np.dot(b2, G11))
-
 mu1, mu2, l = symbols('mu1 mu2 l')
 q1, q2 = symbols('q1 q2')
 
@@ -136,18 +75,39 @@ particles = [particle1, particle2]
 
 lagrange = Lagrange(particles, freedom_degrees)
 
+print lagrange.I
+print lagrange.a
+print lagrange.A
+
 mu1_value = 14648.
 mu2_value = 38363.
 l_value = 4.39
+
+print lagrange.A[1, 0].subs({l: 1.0, mu1: 2.0})
 
 lagrange.I = lagrange.I.subs({mu1: mu1_value, mu2: mu2_value, l: l_value})
 lagrange.a = lagrange.a.subs({mu1: mu1_value, mu2: mu2_value, l: l_value})
 lagrange.A = lagrange.A.subs({mu1: mu1_value, mu2: mu2_value, l: l_value})
 
-hamiltonian = Hamiltonian(lagrange.I, lagrange.a, lagrange.A)
+print lagrange.I
+
+I = Lambdify([q1, q2], lagrange.I)
+a = Lambdify([q1, q2], lagrange.a)
+A = Lambdify([q1, q2], lagrange.A)
+
+attempts = int(1e5)
 
 t0 = clock()
+for i in range(attempts):
+    s = I([2., 3.])
+print 'Needed: {0}s'.format((clock() - t0) / attempts)
 
-for i in range(10000):
-    s = hamiltonian.G11_eval([1., 2.], hamiltonian.I_derivatives, hamiltonian.a_derivatives, hamiltonian.A_derivatives)
-print 'Time needed" {0}s'.format((clock() - t0) / 10000)
+t0 = clock()
+for i in range(attempts):
+    s = a([2., 3.])
+print 'Needed: {0}s'.format((clock() - t0) / attempts)
+
+t0 = clock()
+for i in range(attempts):
+    s = A([2., 3.])
+print 'Needed: {0}s'.format((clock() - t0) / attempts)
