0b07273e5622db295535012541c93e0f00a63e21
artfin <artfin@mail.ru>
2017-06-18 23:52:37 +0300

    convolution-correlation pdf

--- learning-stuff/python-fft/fft-test.py
+++ learning-stuff/python-fft/fft-test.py
@@ -2,27 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft
 
-# number of samplepoints
-#N = 4 
-# sample spacing
-#T = 1.0
+def fourier_spectrum(X, sample_spacing, min_period):
+    ps = np.abs(fft(X))**2
+    freqs = np.fft.fftfreq(X.size, sample_spacing)
+    idx = np.argsort(freqs)
 
-#x = np.linspace(0.0, N * T, N)
-#y = np.sin(50.0 * 2.0 * np.pi * x) + 0.5 * np.sin(80.0 * 2.0 * np.pi * x)
-#yf = fft(y)
-#xf = np.linspace(0.0, 1.0 / (2.0 * T), N/2)
+    plt.plot(freqs[idx], ps[idx])
+    plt.show()
 
-#print(x)
-#print(y)
-#print('-'*30)
-#print(xf)
-#print(yf)
+ #number of samplepoints
+N = 200 
+ #sample spacing
+T = 1.0
 
-x = [1.0, 2.0, 3.0, 4.0]
-y = fft(x)
+x = np.linspace(0.0, N * T, N)
+y = np.sin(2.0 * np.pi * x) 
+yf = fft(y)
+xf = np.linspace(0.0, 1.0 / (2.0 * T), N/2)
 
-print(y) 
 #fig, ax = plt.subplots()
 #ax.plot(xf, 2.0/N * np.abs(yf[:N//2]))
 #plt.show()
 
+fourier_spectrum(y, x, 0.01)
