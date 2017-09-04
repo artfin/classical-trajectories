e70414da33abc5bc46c6fae2b6af02bf81235b4b
artfin <artfin@mail.ru>
2017-09-04 19:24:37 +0300

    interim

---
+++ random_generators/diatomics_distr.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <random>
+
+#include <Eigen/Dense>
+
+using namespace std;
+
+random_device rd;
+mt19937 eng( rd() );
+uniform_real_distribution<double> distr( 0.0, 1.0 );
+
+void RMatrix( Matrix3d &m )
+{
+
+}
+
+int main()
+{
+    return 0;
+}
